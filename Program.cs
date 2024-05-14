d2491a915943cf9c9c398035dda8e4e6b8bffafd
Fernando <ibanez.fernando.2018@gmail.com>
2024-05-14 13:49:12 -0300

    Cuarto commit

--- Program.cs
+++ Program.cs
@@ -6,9 +6,10 @@ a = 10;
 b = a;
 Console.WriteLine("valor de a:" + a);
 Console.WriteLine("valor de b:" + b);
+//Iniciando punto 1
 int num1;
-int num2;
 int auxNum;
+int num2 = 1;
 string linea;
 do
 {
@@ -16,11 +17,14 @@ do
     linea = Console.ReadLine();
     if (int.TryParse(linea, out num1))
     {
-        Console.WriteLine("Numero aceptado" + num1);
+        Console.WriteLine("Numero aceptado: " + num1);
+        Console.WriteLine("Ingrese un segundo valor");
         linea = Console.ReadLine();
         if (int.TryParse(linea, out num2))
         {
-            Console.WriteLine("Numero aceptado" + num2);
+            Console.WriteLine("Numero aceptado: " + num2);
+            Console.WriteLine("Num 1: " + num1);
+            Console.WriteLine("Num 2: " + num2);
             auxNum = num2;
             num2 = num1;
             num1 = auxNum;
@@ -34,4 +38,7 @@ do
     {
         Console.WriteLine("Numero no valido");
     }
-} while ((num1 < 0) && (num2 < 0));
+} while (num1 < 0);
+
+Console.WriteLine("Num 1 ahora: " + num1);
+Console.WriteLine("Num 2 ahora: " + num2);
