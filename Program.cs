4c90e146794732cbb346ac7f4b010e4b57d99ed5
Fernando <ibanez.fernando.2018@gmail.com>
2024-05-14 13:04:17 -0300

    segundo commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a;
+int b;
+a = 10;
+b = a;
+Console.WriteLine("valor de a:" + a);
+Console.WriteLine("valor de b:" + b);
+int num1;
+int num2;
+do
+{
+    Console.WriteLine("Ingrese un valor");
+    
+} while (num1 < 0 && num2 < 0);
