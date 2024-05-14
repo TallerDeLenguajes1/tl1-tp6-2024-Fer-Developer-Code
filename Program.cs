3423d2c6185522bd90c731d2950cdcde9c1fd90f
Fernando <ibanez.fernando.2018@gmail.com>
2024-05-14 13:21:23 -0300

    3 commit

--- Program.cs
+++ Program.cs
@@ -8,8 +8,30 @@ Console.WriteLine("valor de a:" + a);
 Console.WriteLine("valor de b:" + b);
 int num1;
 int num2;
+int auxNum;
+string linea;
 do
 {
     Console.WriteLine("Ingrese un valor");
-    
-} while (num1 < 0 && num2 < 0);
+    linea = Console.ReadLine();
+    if (int.TryParse(linea, out num1))
+    {
+        Console.WriteLine("Numero aceptado" + num1);
+        linea = Console.ReadLine();
+        if (int.TryParse(linea, out num2))
+        {
+            Console.WriteLine("Numero aceptado" + num2);
+            auxNum = num2;
+            num2 = num1;
+            num1 = auxNum;
+        }
+        else
+        {
+            Console.WriteLine("Numero no valido");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Numero no valido");
+    }
+} while ((num1 < 0) && (num2 < 0));
